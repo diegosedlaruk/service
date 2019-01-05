0a97bfff1ffdb4d6b1fb1cc9caab11aadab6bd80
diego <diego.sedlaruk@live.com.ar>
2019-01-05 13:05:50 -0300

    first commit

---
+++ src/main/java/com/eduit/service/AutoResource.java
@@ -0,0 +1,14 @@
+package com.eduit.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutoResource {
+
+    @GetMapping(path = "basicInfo", produces = "application/json")
+    public AutoResponse getBasicInfo(){
+        return new AutoResponse("emma","sedlaruk",2);
+    }
+}
---
+++ src/main/java/com/eduit/service/AutoResponse.java
@@ -0,0 +1,39 @@
+package com.eduit.service;
+
+public class AutoResponse {
+
+
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+
+    public AutoResponse(String name, String apellido, Integer edad){
+        this.nombre = name;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+}
